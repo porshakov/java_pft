7ec0f87f26a971e3711ac6bdef379bd846df4741
porshakov <56513340+porshakov@users.noreply.github.com>
2019-10-25 00:28:04 +0300

    Добавлены тесты площади квадрата и расстояния между точками

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+import static ru.stqa.pft.sandbox.Point.distance;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DistanceTests {
+
+  @Test
+  public void testDistance1() {
+    Point p = new Point(10,13,15,19);
+    Assert.assertEquals(p.distance(), 5);
+  }
+
+  @Test
+  public void testDistance2() {
+    //Point p = new Point(8,1,15,19);
+    Point p1 = new Point(7,10);
+    Point p2 = new Point(18,22);
+    Assert.assertEquals(distance(p1, p2), 5);
+  }
+}
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class SquareTests {
+
+  @Test
+  public void testAreaL5(){
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 25.0);
+  }
+
+  @Test
+  public void testAreaL99(){
+    Square s = new Square(99);
+    Assert.assertEquals(s.area(), 9801.0);
+  }
+}
