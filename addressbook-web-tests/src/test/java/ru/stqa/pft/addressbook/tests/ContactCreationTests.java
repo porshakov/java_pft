e5667ec15b0cc9b8630d3d9fce2a14560ad6be3b
porshakov <56513340+porshakov@users.noreply.github.com>
2020-06-28 23:15:59 +0300

    Реализована проверка и выполнение предусловий для теста модификации контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,14 +3,15 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.By;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void TestContactCreation() throws Exception{
-    //app.getContactHelper().initContactCreation();
-    //app.getContactHelper().fillContactForm();
-    //app.getContactHelper().submitContactCreation();
+    if(!app.getGroupHelper().isThereGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getContactHelper().createContact(new ContactData("John", "F.", "Smith", "user1", "+134637543","test@mail.com", "test1"), true);
     app.getNavigationHelper().gotoHomePage();
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,6 +8,11 @@ public class ContactModificationTests extends TestBase{
   public void testContactModification(){
     app.getNavigationHelper().gotoContactPage();
     app.getContactHelper().searchContact("Smith");
+    if(! app.getContactHelper().isThereContact()) {
+      app.getContactHelper().createContact(new ContactData("John", "F.", "Smith", "user1", "+134637543", "test@mail.com", "test1"), true);
+      app.getNavigationHelper().gotoContactPage();
+      app.getContactHelper().searchContact("Smith");
+    }
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("1", "2", "3", "", "", "", null), false);
     app.getContactHelper().submitContactModification();
