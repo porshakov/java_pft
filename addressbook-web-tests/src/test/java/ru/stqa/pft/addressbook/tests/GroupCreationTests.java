7ef228740508385fac5293d2feaf0a3e59561ad3
porshakov <56513340+porshakov@users.noreply.github.com>
2020-07-07 00:18:36 +0300

    test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -15,6 +15,6 @@ public class GroupCreationTests extends TestBase{
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() + 1);
+    Assert.assertEquals(after.size(), before.size() + 1);  
   }
 }
