79404cd00960ef67d0ee438fa33907fbec9f9997
porshakov <56513340+porshakov@users.noreply.github.com>
2020-07-08 00:46:59 +0300

    Добавлено сравнение списка групп в виде коллекций до удаления и после

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String name;
   private final String header;
@@ -15,8 +17,28 @@ public class GroupData {
     return name;
   }
 
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+
   public String getHeader() {
     return header;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
   }
 
   public String getFooter() {
